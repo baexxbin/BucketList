1faed0a98537b673d8bd018d6345d9944f1475b4
baexxbin <soo3886@gmail.com>
2021-12-30 16:27:11 +0900

    connect App addlist

--- bucketlistProject/addlist/views.py
+++ bucketlistProject/addlist/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Buckets
+from django.http import HttpResponse
 
 # Create your views here.
+def addlist(request):
+    return HttpResponse("addlist page")
+    # buckets = Buckets.objects
+    # return render(request, 'addlist.html')
--- bucketlistProject/home/models.py
+++ bucketlistProject/home/models.py
@@ -4,9 +4,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Buckets(models.Model):
+    bk_idx = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    target_at = models.DateTimeField()
     completed_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
